ad1ae0f3aecf7b70bf359ea808765263af08768f
dhruvrajsinghgaur <rajeshreegaur42@gmail.com>
2026-05-30 22:00:08 +0530

    Added Solutions for LC_76 and LC_125

--- src/Patterns/SlidingWindow/LC_76.java
+++ src/Patterns/SlidingWindow/LC_76.java
@@ -1,10 +1,5 @@
 package Patterns.SlidingWindow;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class LC_76 {
     public String minWindow(String s, String t) {
         if (s.length() < t.length()) return "";
--- src/Patterns/TwoPointer/LC_125.java
+++ src/Patterns/TwoPointer/LC_125.java
@@ -1,6 +1,33 @@
 package Patterns.TwoPointer;
 
 public class LC_125 {
+
+    // TC: O(n).
+    // SC: O(1).
+    public boolean isPalindrome(String s){
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right){
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) return false;
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    // TC: O(n).
+    // SC: O(n).
     public boolean isPalindrome1(String s) {
         StringBuilder forwardStr = new StringBuilder();
         StringBuilder backwardStr = new StringBuilder();
@@ -11,12 +38,12 @@ public class LC_125 {
             boolean addAllowed2 = true;
             if (ch1 < 97 || ch1 > 122){
                 if (ch1 >= 65 && ch1 <= 90) ch1 = (char) (ch1 + 32);
-                else if(ch1 >= 48 && ch1 <= 57);
+                else if(ch1 >= 48 && ch1 <= 57){}
                 else addAllowed1 = false;
             }
             if (ch2 < 97 || ch2 > 122){
                 if (ch2 >= 65 && ch2 <= 90) ch2 = (char) (ch2 + 32);
-                else if(ch2 >= 48 && ch2 <= 57);
+                else if(ch2 >= 48 && ch2 <= 57){}
                 else addAllowed2 = false;
             }
 
